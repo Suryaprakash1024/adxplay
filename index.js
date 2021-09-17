b13aa68e22d83abf60e28924524b5767150fc9d8
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2021-09-17 14:45:46 +0000

    empty data fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const hbs = require('hbs')
 const request = require('request');
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 
 app.set('view engine','hbs')
 app.set('views',path.join(__dirname,'./views'))
@@ -25,23 +25,31 @@ app.get('/share-video',(req,res)=>{
         })
     }
     else{
-        var link = 'https://pdisk1.net/api/ndisk-api/content/detail?item_id='+req.query.videoid;
+        console.log(req.query.videoid)
+        var link = 'https://cofilink.com/api/ndisk-api/content/detail?item_id='+req.query.videoid+'uid=51081852&user_ip=1.0.0.1';
         const url = encodeURI(link);
         request({url,json:true},(error,{body}) => 
                     {
+                        // console.log(url);
+            console.log(body);
+            if(body.data == ''){
+                res.render('error')
+            }
+            else{
                         var hrs = Math.floor((body.data.attachments[0].duration/3600) % 24);
                         var mins = Math.floor((body.data.attachments[0].duration/60) % 60);
                         var secs = Math.floor((body.data.attachments[0].duration) % 60);
                         var size = formatFileSize(body.data.attachments[0].videos[0].file_size)
-                        console.log(body.data.title)
+                        console.log(body.data.attachments[0].videos,":",body.data.attachments[0].videos[0].url)
                         //console.log(body.explanation);
                         res.render('video',{
                         content: link,
                         title: body.data.title,
                         hrs,mins,secs,size,
                         poster:body.data.attachments[0].cover,
-                        url: body.data.attachments[0].videos[0].url
-                    }) 
+                        url: body.data.attachments[0].videos[0].url+"?token=cbebfc7cdb998d5ec2ea124c02d4e0b7"
+                    })
+            }
          });
         
     }
@@ -57,5 +65,5 @@ function formatFileSize(bytes,decimalPoint) {
 }
 
 app.listen(port,()=>{
-    console.log('Its running')
+    console.log('Its running on '+ port)
 })
